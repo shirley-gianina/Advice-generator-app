d8eb8f75dd498edc9e4e063aa7258fc699f41dbc
Shirley Gianina <shirley.cd853f@Shirleys-MacBook-Pro.local>
2022-04-01 01:32:51 +0200

    fix: footer

--- src/components/Attributions/Attributions.jsx
+++ src/components/Attributions/Attributions.jsx
@@ -3,27 +3,25 @@ import "./Attributions.css";
 
 const Attributions = () => {
   return (
-    <footer>
-      <blockquote className="attribution">
-        Challenge by{" "}
-        <a
-          href="https://www.frontendmentor.io/profile/shirley-gianina"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Frontend Mentor
-        </a>
-        . Coded by{" "}
-        <a
-          href="https://github.com/shirley-gianina"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Shirley Gianina
-        </a>
-        .
-      </blockquote>
-    </footer>
+    <blockquote className="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io/profile/shirley-gianina"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://github.com/shirley-gianina"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Shirley Gianina
+      </a>
+      .
+    </blockquote>
   );
 };
 
